e16c483fc6e092baa5a6ececb57a560618203308
OlfaBabai <61467506+OlfaBabai@users.noreply.github.com>
2020-03-03 14:55:39 +0100

    new

---
+++ Wimbee_Hiring.Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Wimbee_Hiring.Models
+{
+   public class Person
+    {
+        
+        public int IdPerson { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+    }
+}
---
+++ Wimbee_Hiring.Persistence/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using Wimbee_Hiring.Models;
+
+namespace Wimbee_Hiring.Persistence
+{
+    public class AppDBContext : DbContext
+    {
+        
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Write Fluent API configurations here
+
+
+        }
+    }
+}
