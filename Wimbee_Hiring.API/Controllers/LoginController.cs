59f2c3b01b21dbddebdf55b1557f9980b4f10795
OlfaBabai <61467506+OlfaBabai@users.noreply.github.com>
2020-06-26 08:25:52 +0200

    JwtLogin

--- Wimbee_Hiring.API/Controllers/LoginController.cs
+++ Wimbee_Hiring.API/Controllers/LoginController.cs
@@ -8,6 +8,12 @@ using Wimbee_Hiring.Models;
 using Wimbee_Hiring.Service;
 using Wimbee_Hiring.Service.Interfaces;
 using Wimbee_Hiring.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Wimbee_Hiring.API.Controllers
 {
@@ -15,28 +21,100 @@ namespace Wimbee_Hiring.API.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        public IConfiguration _config;
         private readonly PersonRepository personRepository;
+        private readonly IGenericRepository<Person> person;
 
-        public LoginController()
+        public LoginController(IConfiguration configuration, IGenericRepository<Person> p)
         {
-             personRepository = new PersonRepository (null);
+            _config = configuration;
+            person = p;
         }
 
-        [HttpPost]
-        public Person Verifier(string email, string pwd)
+        [HttpGet("{email}/{pwd}")]
+        public IActionResult Login(string email, string pwd)
         {
-            Person p = new Person();
-            p = personRepository.GetByEmail(email);
-            //if (p!=null)
-            //{ 
-            //    if (p.Password == pwd)
-            //    { 
-            //        return true; 
-            //    }
-            //    else return false;
-            //}
-            //else return false;
-            return p;
+            IActionResult response = Unauthorized();
+            var user = Authentificate(email, pwd);
+            if (user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
         }
+
+        [HttpGet]
+        private Person Authentificate(string email, string pwd)
+        {
+            Person user = null;
+            IEnumerable<Person> people = person.GetAll();
+            foreach (Person per in people)
+            {
+                if (per.Email == email && per.Password == pwd)
+                {
+                    user = per;
+                }
+            }
+            return user;
+        }
+
+        private string GenerateJSONWebToken(Person userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]{
+            new Claim(JwtRegisteredClaimNames.Sub,userinfo.FirstName+userinfo.LastName),
+            new Claim(JwtRegisteredClaimNames.Email, userinfo.Email),
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+                );
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return encodetoken;
+        }
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Bienvenu(e)" + userName;
+        }
+
+        //[HttpGet("{email}")]
+        //public Person SearchEmail(string email)
+        //{
+        //    return personRepository.GetByEmail(email);
+        //}
+
+        //public bool Verifier(string email, string pwd)
+        //{
+        //    bool test;
+        //    Person p = new Person();
+        //    p = personRepository.GetByEmail(email);
+        //    if (p != null)
+        //    {
+        //        if (p.Password == pwd)
+        //        {
+        //            test=true;
+        //        }
+        //        else test=false;
+        //    }
+        //    else test=false;
+        //    return test;
+        //}
+
     }
 }
--- Wimbee_Hiring.API/Startup.cs
+++ Wimbee_Hiring.API/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Wimbee_Hiring.Models;
 using Wimbee_Hiring.Persistence;
 using Wimbee_Hiring.Service;
@@ -38,8 +41,28 @@ namespace Wimbee_Hiring.API
             services.AddScoped<IGenericRepository<Person>, GenericRepository<Person>>();
             services.AddScoped<IGenericRepository<Ticket>, GenericRepository<Ticket>>();
             services.AddScoped<IGenericRepository<Person>, GenericRepository<Person>>();
+            
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials().Build());
 
+            });
+            
             services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +76,8 @@ namespace Wimbee_Hiring.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
